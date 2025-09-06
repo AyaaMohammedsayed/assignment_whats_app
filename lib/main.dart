52fbad2b85ab97051c6cddb633c870ce32a51a61
Aya <ayahame99@gmail.com>
2025-09-06 23:37:20 +0300

    initial project

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:assignment_whats_app/chat_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(WhatsApp());
+}
+
+class WhatsApp extends StatelessWidget {
+  const WhatsApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: ChatScreen(),
+    );
+  }
+}
